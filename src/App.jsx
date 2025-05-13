238000f92450a78e112eb8a133996e63f80ace2d
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-13 15:02:38 +0200

    [UPD] MILESTON 1 completata (mootsrati in console la lista degli attori)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [chracters, setCharacters] = useState([]);
+
+  const showActors = () => {
+    axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => { console.log(response); setCharacters(response.data.results); })
+  }
+
+  useEffect(() => {
+    showActors()
+  }, []);
+
 
   return (
     <>
