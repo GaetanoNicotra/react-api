e873d630a9dae4d47bcb1b9275861568805aa9de
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-13 21:04:02 +0200

    [DONE] Bonus completato

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,35 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-
+  // definizione variabili di stato
   const [characters, setCharacters] = useState([]);
+  const [charactersBonus, setCharactersBonus] = useState([]);
 
+  // Sezione delle chiamate ajax
   const showActors = () => {
     axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => {
       setCharacters(response.data);
       console.log(response.data);
       console.log(characters);
     })
-  }
+  };
 
+  const showActresses = () => {
+    axios.get('https://lanciweb.github.io/demo/api/actresses/').then((response) => {
+      setCharactersBonus(response.data);
+      console.log(response.data);
+      console.log(charactersBonus)
+    })
+  };
+
+  // sezione degli useEffect
   useEffect(() => {
     showActors()
   }, []);
 
+  useEffect(() => {
+    showActresses()
+  }, []);
 
   return (
     <>
@@ -43,7 +57,30 @@ function App() {
                   {character.awards}</li>
 
               </ul>
-            </div>))}
+            </div>
+          ))}
+          {charactersBonus.map((characterB) => (
+            <div key={`characterB-${characterB.id}`} className="card col-2 h-100 me-4">
+              <img src={characterB.image} className="card-img-top" alt="..." />
+              <div className="card-body ">
+                <h5 className="card-title">{characterB.name}</h5>
+                <hr />
+                <p className="card-text"><strong>Biography</strong> <br />{characterB.biography}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                  <div><strong>Birth year</strong></div>
+                  {characterB.birth_year}</li>
+                <li className="list-group-item">
+                  <div><strong>Nationality</strong></div>
+                  {characterB.nationality}</li>
+                <li className="list-group-item">
+                  <div><strong>Awards</strong></div>
+                  {characterB.awards}</li>
+
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </>
