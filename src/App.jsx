2fdfe037c3eca68158b45e526feffdb6913d3343
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-13 15:47:37 +0200

    [UPD] scrittura della struttura della card

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import axios from 'axios'
 
 function App() {
 
-  const [chracters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   const showActors = () => {
-    axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => { console.log(response); setCharacters(response.data.results); })
+    axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => { setCharacters(response.data.results); })
   }
 
   useEffect(() => {
@@ -16,7 +16,23 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <header><h1> Cast Fetching</h1></header>
+      <div className="container">
+        {characters.map((character) => (
+          <div key={`character-${character.id}`} className="card">
+            <img src={character.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{character.name}</h5>
+              <p className="card-text">{character.biography}</p>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">{character.birth_year}</li>
+              <li className="list-group-item">{character.nationality}</li>
+              <li className="list-group-item">{character.awards}</li>
+            </ul>
+          </div>))}
+      </div>
+
     </>
   )
 }
