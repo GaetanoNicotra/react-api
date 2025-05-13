4bb7099f61acea0234b3bc2843ec560cf42d14ed
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-05-13 17:28:16 +0200

    [UPD] Mileston 2 e 3 completate

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   const [characters, setCharacters] = useState([]);
 
   const showActors = () => {
-    axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => { setCharacters(response.data.results); })
+    axios.get('https://lanciweb.github.io/demo/api/actors/').then((response) => {
+      setCharacters(response.data);
+      console.log(response.data);
+      console.log(characters);
+    })
   }
 
   useEffect(() => {
@@ -18,21 +22,22 @@ function App() {
     <>
       <header><h1> Cast Fetching</h1></header>
       <div className="container">
-        {characters.map((character) => (
-          <div key={`character-${character.id}`} className="card">
-            <img src={character.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{character.name}</h5>
-              <p className="card-text">{character.biography}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">{character.birth_year}</li>
-              <li className="list-group-item">{character.nationality}</li>
-              <li className="list-group-item">{character.awards}</li>
-            </ul>
-          </div>))}
+        <div className="row">
+          {characters.map((character) => (
+            <div key={`character-${character.id}`} className="card col-2 h-100">
+              <img src={character.image} className="card-img-top" alt="..." />
+              <div className="card-body ">
+                <h5 className="card-title">{character.name}</h5>
+                <p className="card-text">{character.biography}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">{character.birth_year}</li>
+                <li className="list-group-item">{character.nationality}</li>
+                <li className="list-group-item">{character.awards}</li>
+              </ul>
+            </div>))}
+        </div>
       </div>
-
     </>
   )
 }
